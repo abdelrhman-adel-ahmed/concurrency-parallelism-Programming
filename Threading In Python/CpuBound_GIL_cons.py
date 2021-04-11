d11ba3965f7c2ea8f18f8dbc39e97de186828762
abdelrhman-adel-ahmed <abdelrhman.bedo007@gmail.com>
2021-04-11 15:12:37 +0200

    Create CpuBound_GIL_cons.py

---
+++ Threading In Python/CpuBound_GIL_cons.py
@@ -0,0 +1,39 @@
+import time
+from threading import Thread
+
+#acquire and release overheads added by the GIL while doing cpu bound tasks will increase the time more than one threaded programme !! :(.
+
+
+COUNT = 50000000
+
+
+def countdown(n):
+    while n > 0:
+        n -= 1
+
+
+start = time.time()
+countdown(COUNT)
+end = time.time()
+
+print("Time taken in seconds -", end - start)
+
+
+#--------------------------------------------------------------------------------
+COUNT = 50000000
+
+def countdown(n):
+    while n > 0:
+        n -= 1
+
+
+t1 = Thread(target=countdown, args=(COUNT // 2,))
+t2 = Thread(target=countdown, args=(COUNT // 2,))
+
+start = time.time()
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+end = time.time()
+print("Time taken in seconds -", end - start)
